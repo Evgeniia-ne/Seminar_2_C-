a70275aa32df47ef8be80b049dcf695221075955
Asus <koncarik@mail.ru>
2022-11-28 22:53:35 +0500

    first commit

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int number = new Random().Next(10, 100);
+Console.WriteLine("Случайное число: " + number);
+
+int ostatok = number % 10;
+Console.WriteLine("Последняя цифра: " + ostatok);
+
+int first = number / 10;
+Console.WriteLine("Первая цифра: " + first);
+if (ostatok > first)
+{
+    Console.WriteLine("Наибольшее число: " + ostatok);
+}
+else
+{
+    Console.WriteLine("Наибольшее число: " + first);
+}
