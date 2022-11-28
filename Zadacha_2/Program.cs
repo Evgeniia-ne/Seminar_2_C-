76be692bbfc7e9619da17478100e3c2541f82651
Asus <koncarik@mail.ru>
2022-11-28 22:54:12 +0500

    second commit

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+int number = new Random().Next(100, 999);
+Console.WriteLine("Случайное число: " + number);
+
+int first = number / 100;
+Console.WriteLine("Первая цифра: " + first);
+
+int third = number % 10;
+Console.WriteLine("Третья цифра: " + third);
+
+Console.WriteLine("Число без второй цифры: " + first + third);
