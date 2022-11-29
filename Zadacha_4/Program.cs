92f39fb468168c1c1aded87ae80724d1dc7245be
Asus <koncarik@mail.ru>
2022-11-29 19:11:22 +0500

    OK

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+if (number%7 == 0 && number%23 == 0) Console.Write($"{number} -> да");
+else Console.Write($"{number} - > нет" );
