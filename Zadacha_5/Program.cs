37c41088a6a3290c347cf88e40f5676213c08e19
Asus <koncarik@mail.ru>
2022-11-29 20:34:17 +0500

    OK

---
+++ Zadacha_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите первое число ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1*number1 == number2 ^ number2*number2 == number1)
+{
+    Console.Write($"{number1}, {number2} -> да");
+}
+else
+Console.Write($"{number1}, {number2} - > нет" );
